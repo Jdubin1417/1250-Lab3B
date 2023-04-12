e7f8677966a32d2fd6612941b46ef0b1354eb208
Justin Nicholas Dubin <99613905+Jdubin1417@users.noreply.github.com>
2023-04-12 09:01:42 -0400

    Add files via upload

---
+++ Lab3B/RestaurantBill/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+string mealChargeAsString;
+int mealCharge;
+//
+Console.Write("Enter Charge for Meal:");
+mealChargeAsString = Console.ReadLine();
+mealCharge = Convert.ToInt32(mealChargeAsString);
+//
+const double TAX_RATE = 0.0675;
+double tax = mealCharge * TAX_RATE;
+double tip = mealCharge * 0.2;
+double total = mealCharge + tax + tip;
+//
+Console.Write("Your meal charge is $" + mealCharge + ", your tax amount is $" + tax + ",  your tip amount is $" + tip + ", and your total bill is $" + total + ".");
---
+++ Lab3B/ShelterCats/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+string maleCatsAsString;
+string femaleCatsAsString;
+int maleCats;
+int femaleCats;
+//
+Console.Write("How many male cats are in the shelter?");
+maleCatsAsString = Console.ReadLine();
+Console.Write("How many female cats are in the shelter?");
+femaleCatsAsString = Console.ReadLine();
+maleCats = Convert.ToInt32(maleCatsAsString);
+femaleCats = Convert.ToInt32(femaleCatsAsString);
+//
+float totalCats = maleCats + femaleCats;
+float malePercent = maleCats / totalCats * 100;
+float femalePercent = femaleCats / totalCats * 100;
+//
+Console.Write("In the shelter, " + malePercent + "% of cats are male, " + femalePercent + "% of cats are female.");
